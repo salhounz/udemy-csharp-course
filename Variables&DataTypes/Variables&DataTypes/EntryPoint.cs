fce37d05345e06ccc727591ef860162a5bf8120e
salhounz <codyjsullins@gmail.com>
2023-09-12 13:14:43 -0500

    Section 2.6 and 2.7 introducing variables and different data types

---
+++ Variables&DataTypes/Variables&DataTypes/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        // Declare (Declaration)
+        // introduce a new variable to the program with data type and name
+        int myInt;
+
+        // Instantiate (Instantiation)
+        // creating a new instance of a class
+        // Ex. MyObject test = new MyObject();
+
+        // Initalize (Initilization)
+        // the newInt is declared and initialized with a value of 1000
+        int newInt = 1000;
+
+        // Assign (Assignation)
+        // the variable a is declared and initialized with a value of 1
+        // and is then assigned a value of 2
+        int a = 1;
+        a = 2;
+
+
+        // Overview of Data Types
+        int number = 5;
+        string word = "Hello";
+        char letter = 's';
+        bool isTrue = true;
+        float floatingnumber = 3.14159f;
+        double doublenumber = 3.141593184959;
+
+
+
+
+    }
+}
