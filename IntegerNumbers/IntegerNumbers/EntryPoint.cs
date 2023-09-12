23a69a0047fbe6dd58423b55c1b599a22ae487dd
salhounz <codyjsullins@gmail.com>
2023-09-12 13:32:11 -0500

    Section 2.8 covering signed/unsigned data types, min/max data type values, and bit sizes

---
+++ IntegerNumbers/IntegerNumbers/EntryPoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+// used to minimize boilerplate code
+// Ex. Console.ReadLine() vs. System.Console.ReadLine()
+
+class EntryPoint
+{
+    static void Main()
+    {
+        // holds 32 bit postiive/negative numbers 
+        int myInt = 5;
+
+        // Output Min and Max values for int data type
+        Console.WriteLine(int.MinValue);
+        Console.WriteLine(int.MaxValue);
+
+        // unsigned int
+        // 32 bit positive numbers
+        uint myUInt = 10;
+
+        // unsigned
+        // 8 bit numbers
+        // 0 to 255
+        byte myByte = 255;
+
+        // signed 
+        // 8 bit numbers
+        // - 128 to 127
+        sbyte mySignedByte = -128;
+
+        // signed
+        // 64 bit positive/negative numbers
+        long myLong = 1234123412343;
+
+        // unsigned
+        // 64 bit positive numbers
+        ulong myUnsignedLong = 123412341234;
+
+    }
+}
