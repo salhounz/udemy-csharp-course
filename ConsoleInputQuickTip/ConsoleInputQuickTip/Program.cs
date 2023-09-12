f871db6669813f5c5974c4403f1ac40be15778f0
salhounz <codyjsullins@gmail.com>
2023-09-12 12:38:53 -0500

    Adding Section1.5 regarding console input quick tips along with single and multi line comments

---
+++ ConsoleInputQuickTip/ConsoleInputQuickTip/Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+        //Single Line Comment
+
+        /*
+         *  Multi Line Comment
+         */
+    {
+        int age = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Please enter an age to continue: ");
+            age = int.Parse(Console.ReadLine());
+
+            if (age <= 21 && age >= 10)
+            {
+                break;
+            }
+
+            Console.WriteLine("Please input an age between 10 and 21!\n");
+            Thread.Sleep(1000);
+
+        }
+
+        Console.WriteLine($"You selected a correct age of {age}!");
+
+        
+
+    }
+}
