32acdfb74b94b726c1960d8ceccd6912ab6c4cce
salhounz <codyjsullins@gmail.com>
2023-09-13 13:18:06 -0500

    Updating Section 6.17 and 6.18 with example of While and For Loops

--- IfElseIfElseIf/IfElseIfElseIf/EntryPoint.cs
+++ IfElseIfElseIf/IfElseIfElseIf/EntryPoint.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your first number: ");
+        int firstNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter your second number: ");
+        int secondNum = int.Parse(Console.ReadLine());
+
+        if (firstNum < secondNum)
+        {
+            Console.WriteLine("Of course, {0} is smaller than {1}", firstNum, secondNum);
+        }
+        else if (firstNum == secondNum)
+        {
+            Console.WriteLine("The numbers are equal!");
+        }
+        else if (true)
+        {
+            // Replace true condition and add code to run if new condition is met
+            // as many times as necessary
+        }
+        else
+        {
+            Console.WriteLine("Oh yeah, {0} is smaller than {1}", firstNum, secondNum);
+        }
+    }
+}
---
+++ WhileLoopsForLoops/WhileLoopsForLoops/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main(string[] args)
+    {
+
+        int firstNum = 0;
+        int secondNum = 10;
+        // While Loop printing from 0-10
+        while(firstNum <= secondNum)
+        {
+            Console.WriteLine("While Loop Output is: " + firstNum);
+            firstNum++;
+        }
+
+        
+        int thirdNum = 11;
+
+        
+        int fourthNum = 10;
+
+        for(int i = 0; i < fourthNum; i++)
+        {
+            Console.WriteLine("For Loop Output is: " + (thirdNum + i));
+        }
+
+    }
+}
